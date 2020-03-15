5a6537a26fc4766c2afb227c58fe395a2fe451bb
shenjiahuan <shenjiahuan@sjtu.edu.cn>
2020-03-15 14:09:06 +0800

    add samba support

---
+++ samba/entrypoint.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+adduser -s /sbin/nologin -h /home/samba -H -D $SMB_ADMIN_USER
+(echo $SMB_ADMIN_PASS; echo $SMB_ADMIN_PASS) | smbpasswd -a $SMB_ADMIN_USER
+adduser -s /sbin/nologin -h /home/samba -H -D $SMB_GUEST_USER
+(echo $SMB_GUEST_PASS; echo $SMB_GUEST_PASS) | smbpasswd -a $SMB_GUEST_USER
+
+sh -c "sed -i 's/admin_user/$SMB_ADMIN_USER/g' /etc/samba/smb.conf"
+sh -c "sed -i 's/guest_user/$SMB_GUEST_USER/g' /etc/samba/smb.conf"
+
+smbd --foreground --log-stdout
